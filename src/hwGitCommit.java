3b0772730e62436a118b94485d9e34d25ced73dd
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-03 18:06:31 +0300

    add main Method

--- src/hwGitCommit.java
+++ src/hwGitCommit.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -45,12 +48,38 @@ public class hwGitCommit {
 
     Season season = switch (month) {
       case DECEMBER, JANUARY, FEBRUARY -> Season.WINTER;
-      // после return не нужен break
       case MARCH, APRIL, MAY -> Season.SPRING;
       case JUNE, JULY, AUGUST -> Season.SUMMER;
       case SEPTEMBER, OCTOBER, NOVEMBER -> Season.AUTUMN;
     };
     return season;
   }
+  public static void main(String[] args) throws IOException {
+    BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
 
+    System.out.print("Enter the month: ");
+    String month = inputReader.readLine();
+    Month monthToLook = Month.valueOf(month.toUpperCase());
+
+    Map<Month, Integer> monthDays = buildMonthsMap();
+
+    if (monthDays.containsKey(monthToLook)) {
+
+      System.out.printf("The month '%s' contains %d days%n", month, monthDays.get(monthToLook));
+
+      System.out.printf("The month '%s' has ordinal number %d%n", month, monthToLook.ordinal() + 1);
+
+
+      System.out.printf("This month is a part of a '%s' season%n", findSeason(monthToLook));
+
+      int nextMonthIndex = (monthToLook.ordinal() + 1);
+      Month nextMonth = Month.values()[nextMonthIndex % Month.values().length];
+
+      System.out.printf("The next month is '%s'%n", nextMonth);
+      System.out.printf("It has %d days", monthDays.get(nextMonth));
+    } else {
+      System.out.println("No such month: " + month);
+    }
+  }
 }
+
