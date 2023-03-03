bd969a663ccb52bb6ff12ed1c13217602a57f3ed
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-03 18:00:44 +0300

    add buildMonthsMap

--- src/hwGitCommit.java
+++ src/hwGitCommit.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class hwGitCommit {
 
   enum Month {
@@ -20,5 +23,22 @@ public class hwGitCommit {
     SUMMER,
     FALL,
   }
+  public static Map<Month, Integer> buildMonthsMap() {
+    Map<Month, Integer> months = new HashMap<>();
 
+    months.put(Month.JANUARY, 31);
+    months.put(Month.FEBRUARY, 28);
+    months.put(Month.MARCH, 31);
+    months.put(Month.APRIL, 30);
+    months.put(Month.MAY, 31);
+    months.put(Month.JUNE, 30);
+    months.put(Month.JULY, 31);
+    months.put(Month.AUGUST, 31);
+    months.put(Month.SEPTEMBER, 30);
+    months.put(Month.OCTOBER, 31);
+    months.put(Month.NOVEMBER, 30);
+    months.put(Month.DECEMBER, 31);
+
+    return months;
+  }
 }
